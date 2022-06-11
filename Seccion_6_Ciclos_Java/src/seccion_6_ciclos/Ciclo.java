719095a42a44155d4bf15c9fb86ac6a76eb4ccf2
Adrian <luiscabreraucme@hotmail.com>
2022-06-11 00:52:07 -0500

    Merge branch 'friend' of https://github.com/AdrianC47/Java into friend

---
+++ Seccion_6_Ciclos_Java/src/seccion_6_ciclos/Ciclo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seccion_6_ciclos;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Ciclo {
+
+    public static void main(String[] args) {
+        var contador = 0;
+        while( contador <3){
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+    }
+}
