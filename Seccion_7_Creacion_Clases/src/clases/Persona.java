13d54f1a3935d8e91be79e8a3a129a04b9a3eb8e
Adrian <luiscabreraucme@hotmail.com>
2022-06-21 11:44:16 -0500

    Do While

---
+++ Seccion_7_Creacion_Clases/src/clases/Persona.java
@@ -0,0 +1,17 @@
+package clases;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Persona {
+    //Atributos de  la clase
+    String nombre;
+    String apellido;
+    
+    public  void desplegarInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+    
+}
